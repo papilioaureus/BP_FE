e87395979336acead17ef686781218052049cb3a
papilio <papilioaureus@yahoo.com>
2023-10-15 15:53:10 +0200

    kujythrgyhgfd

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,24 +4,23 @@
     <!-- Set a header tag with 3 buttons; Accounts that should lead to the /accounts page, Skull that should lead to the /skull page and Home Page one that leads to /-->
     <header>
       <nav>
-        <a href="/" class="headerElem">Home Page</a>
         <a href="/accounts" class="headerElem">Accounts</a>
         <a href="/skull" class="headerElem">Skull</a>
+        <a href="/" class="headerElem">Go To Home Page</a>
       </nav>
     </header>
 
-    <h1 id="title" class="text">Home Page</h1>
-    <p id="shortDescription" class="text">Welcome to the IE Bank</p>
-    <!-- Add an Image -->
-    <img src="../assets/logo.png" alt="IE Bank" width="300" height="300" />
+    <h1 id="title" class="text">Welcome to IE Bank!</h1>
+    <p id="shortDescription" class="text">Your Trusted Banking Partner</p>
+    <img src="../assets/header-image.png" alt="IE Bank" width="300" height="300" />
 
     <nav id="buttonNav">
-      <router-link to="/accounts">
-        <button type="button" class="btn btn-secondary">Accounts</button>
+      <router-link to="/skull">
+        <button type="button" class="btn btn-secondary">What's the Skull?</button>
       </router-link>
 
-      <router-link to="/skull">
-        <button type="button" class="btn btn-secondary">Skull</button>
+      <router-link to="/accounts">
+        <button type="button" class="btn btn-secondary">Click Here to Access Accounts</button>
       </router-link>
     </nav>
   </div>
@@ -29,7 +28,7 @@
 
 <style scoped>
 header {
-  background-color: #f1f1f1;
+  background-color: #cde9f9;
   padding: 20px;
   text-align: center;
   z-index: 1;
@@ -37,13 +36,13 @@ header {
 
 .headerElem {
   padding: 20px;
-  text-decoration: none;
-  font-size: 20px;
+  text-decoration: underline;
+  font-size: 30px;
   color: black;
 }
 
 .headerElem:hover {
-  background-color: #ddd;
+  background-color: #4242e8;
   color: black;
 }
 
@@ -52,7 +51,7 @@ header {
 }
 
 #title {
-  margin-top: 30px;
+  margin-top: 40px;
   font-size: 50px;
 }
 
@@ -73,11 +72,10 @@ button {
   margin-left: 10px;
   margin-right: 10px;
   padding: 10px;
-  font-size: 20px;
+  font-size: 30px;
   text-align: center;
 
-  /* Make the buttons the same size regardless of the text */
-  width: 100px;
-  height: 50px;
+  width: 150px;
+  height: 100px;
 }
 </style>
