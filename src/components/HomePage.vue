532eb9e7677925aa6278c4e5ea4f8b1222efa109
papilio <papilioaureus@yahoo.com>
2023-10-15 15:43:17 +0200

    yjhgrfe4t5y6hgf

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,98 +1,83 @@
+<!-- Home Page -->
 <template>
-  <div id="app">
-    <header class="header">
-      <h1>IE Online Banking Services</h1>
+  <div>
+    <!-- Set a header tag with 3 buttons; Accounts that should lead to the /accounts page, Skull that should lead to the /skull page and Home Page one that leads to /-->
+    <header>
+      <nav>
+        <a href="/" class="headerElem">Home Page</a>
+        <a href="/accounts" class="headerElem">Accounts</a>
+        <a href="/skull" class="headerElem">Skull</a>
+      </nav>
     </header>
-    <template>
-      <div class="button-container">
-        <router-link to="/skull" class="button">Skull</router-link>
-        <router-link to="/accounts" class="button">Account Management</router-link>
-      </div>
-    </template>
-    <section class="section">
-      <h2>Our Mission Statement</h2>
-      <p>Get Github and Azure to Cooperate For Once In Their Lives</p>
-      <p>Create User Accounts</p>
-    </section>
+
+    <h1 id="title" class="text">Home Page</h1>
+    <p id="shortDescription" class="text">Welcome to the IE Bank</p>
+    <!-- Add an Image -->
+    <img src="../assets/logo.png" alt="IE Bank" width="300" height="300" />
+
+    <nav id="buttonNav">
+      <router-link to="/accounts">
+        <button type="button" class="btn btn-secondary">Accounts</button>
+      </router-link>
+
+      <router-link to="/skull">
+        <button type="button" class="btn btn-secondary">Skull</button>
+      </router-link>
+    </nav>
   </div>
 </template>
 
-
-<script>
-export default {
-  name: 'Home',
-  data() {
-    return {
-      backgroundImage: require('../assets/images/header-image.png')
-    }
-  }
-}
-</script>
-
-
 <style scoped>
-body {
-  font-family: Montserrat, sans-serif;
-}
-
-
-.menu {
-  background-color: #4b93d5;
-  overflow: hidden;
-}
-
-
-.menu ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-}
-
-
-.menu li a {
-  display: block;
-  color: rgb(222, 236, 254);
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-
-.header {
-  padding: 60px;
-  text-align: center;
-  color: rgb(208, 210, 248);
-  background-image: "require(`./assets/images/header-image.png`)";
-  background-size: cover;
-}
-
-
-.section {
+header {
+  background-color: #f1f1f1;
   padding: 20px;
+  text-align: center;
+  z-index: 1;
 }
 
-
-.section h2 {
-  color: #1c5c99;
+.headerElem {
+  padding: 20px;
+  text-decoration: none;
+  font-size: 20px;
+  color: black;
 }
 
+.headerElem:hover {
+  background-color: #ddd;
+  color: black;
+}
 
-.button-container {
+.text {
+  text-align: center;
+}
+
+#title {
+  margin-top: 30px;
+  font-size: 50px;
+}
+
+img {
+  display: block;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#buttonNav {
   display: flex;
   justify-content: center;
   margin-top: 50px;
 }
 
+button {
+  margin-left: 10px;
+  margin-right: 10px;
+  padding: 10px;
+  font-size: 20px;
+  text-align: center;
 
-.button {
-  margin: 0 10px;
-  padding: 10px 20px;
-  background-color: #60a4e5;
-  color: #dbedfe;
-  text-decoration: none;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  font-size: 18px;
+  /* Make the buttons the same size regardless of the text */
+  width: 100px;
+  height: 50px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomePage from "../components/HomePage.vue";
 import Skull from "../components/Skull.vue";
 import AppAccounts from "../components/AppAccounts.vue";
+import HomePage from "../components/HomePage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-
-  {
-    path: "/",
-    name: "HomePage",
-    component: HomePage,
-  },
-
   {
     path: "/skull",
     name: "Skull",
     component: Skull,
   },
-
-
   {
     path: "/accounts",
     name: "AppAccounts",
     component: AppAccounts,
   },
-
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
+  },
 ];
 
 const router = new VueRouter({
